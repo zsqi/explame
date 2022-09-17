820c7b8bd61704337b27240531af633cea34ad47
zsq <1214565968@qq.com>
2022-09-17 09:42:12 +0800

    。

--- LeetCode/LeetCode/StringSeries.cs
+++ LeetCode/LeetCode/StringSeries.cs
@@ -298,5 +298,110 @@ namespace LeetCode
             }
             return count;
         }
+        /// <summary>
+        /// 大数相乘
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public string BigNumberMultiply(string num1, string num2)
+        {
+            // 判断不合法的输入
+            if (num1 == null || num1.Length == 0 || num2 == null || num2.Length == 0)
+            {
+                return null;
+            }
+            // 如果 num1 或 num2 其中有一个为 0， 则最后相乘的结果为 0
+            if ("0".Equals(num1) || "0".Equals(num2))
+            {
+                return "0";
+            }
+
+            char[] chars1 = num1.ToCharArray();
+            char[] chars2 = num2.ToCharArray();
+            // 最后的结果
+            String res = "0";
+            // 相乘之后，后面需要补零的个数
+            int zero = 0;
+            // 从右往左遍历 num2 字符串中的每一个数字， 使其和 num1 字符串相乘， 再相加，即得最后结果
+            for (int i = chars2.Length - 1; i >= 0; i--)
+            {
+                // 每一个数字和 num1 字符串相乘的结果
+                StringBuilder temp = new StringBuilder();
+                // 保存进位
+                int carry = 0;
+
+                // 将后面需要的零补齐
+                for (int j = 0; j < zero; j++)
+                {
+                    temp.Append('0');
+                }
+                zero++;
+
+                // 取出 num2 字符串的每一个数字， 记为 y
+                int y = chars2[i] - '0';
+                // 使 y 和 num1 字符串相乘
+                for (int j = chars1.Length - 1; j >= 0; j--)
+                {
+                    int x = chars1[j] - '0';
+                    int num = x * y + carry;
+                    temp.Append(num % 10);
+                    carry = num / 10;
+                }
+                // 如果最后进位不为 0 ， 则还需要往前进 carry 位
+                if (carry != 0)
+                {
+                    temp.Append(carry);
+                }
+                // 把每次相乘的结果相加起来，就是最后的结果
+                res = BigNumberAdd(res, Reverse(temp.ToString()));
+            }
+            return res;
+        }
+        public String BigNumberAdd(String num1, String num2)
+        {
+            // 判断不合法输入
+            if (num1 == null || num1.Length == 0 || num2 == null || num2.Length == 0)
+            {
+                return null;
+            }
+            // num1 字符串的下标， 从右往左遍历
+            int index1 = num1.Length - 1;
+            // num2 字符串的下标， 从右往左遍历
+            int index2 = num2.Length - 1;
+
+            // 最后相加的结果
+            StringBuilder res = new StringBuilder();
+            // 保存进位
+            int carry = 0;
+
+            // num1 或 num2 只要有一个字符串还有值，就继续循环
+            while (index1 >= 0 || index2 >= 0)
+            {
+                // 拿到 num1 的一个数字
+                int x = index1 >= 0 ? num1[index1--] - '0' : 0;
+                // 拿到 num2 的一个数字
+                int y = index2 >= 0 ? num2[index2--] - '0' : 0;
+                // 拿到的 2 个数字相加，要考虑进位的情况
+                int num = x + y + carry;
+                // 该位只保存个位数
+                res.Append(num % 10);
+                // 十位数要往前进一位
+                carry = num / 10;
+            }
+            // 如果最后进位不为 0 ， 则还需要往前进 carry 位
+            if (carry != 0)
+            {
+                res.Append(carry);
+            }
+            return Reverse(res.ToString());
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
     }
 }
